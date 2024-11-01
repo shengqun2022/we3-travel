a3ebcbb072716b9db07d6d2b1c4c441a088eab21
shengqun.zhu <shengqun2022@gmail.com>
2024-11-01 17:36:09 +0800

    fix: toggle base url

--- src/config/constant.js
+++ src/config/constant.js
@@ -2,11 +2,11 @@
  * @Author: shengqun.zhu shengqun2022@gmail.com
  * @Date: 2024-10-20 18:10:30
  * @LastEditors: shengqun.zhu shengqun2022@gmail.com
- * @LastEditTime: 2024-11-01 10:29:34
+ * @LastEditTime: 2024-11-01 17:35:52
  * @FilePath: /web3-travel/src/const/const.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
 
-const BASE_URL ="http://101.126.153.89:8080"
-// const BASE_URL ="http://18.117.125.253:8080"
+// const BASE_URL ="http://101.126.153.89:8080"
+const BASE_URL ="http://18.117.125.253:8080"
 export { BASE_URL }
