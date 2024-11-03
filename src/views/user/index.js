f109a89fd06bbd58554158edcb4c0c384dfbcd1d
shengqun.zhu <shengqun2022@gmail.com>
2024-11-03 19:32:15 +0800

    fix: follow bug

--- src/views/user/index.js
+++ src/views/user/index.js
@@ -2,7 +2,7 @@
  * @Author: shengqun.zhu shengqun2022@gmail.com
  * @Date: 2024-09-19 16:30:16
  * @LastEditors: shengqun.zhu shengqun2022@gmail.com
- * @LastEditTime: 2024-11-03 19:15:23
+ * @LastEditTime: 2024-11-03 19:30:59
  * @FilePath: /myapp/front/src/views/Mine.js
  * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
  */
@@ -94,6 +94,10 @@ const App = () => {
       url: `${api.userFollow}?${paramsStr}`
     });
     if (res) {
+      messageApi.open({
+        type: "success",
+        content: type==="1" ? '关注成功' : "取关成功" ,
+      });
       setCount(prev=> prev+1)
     }
   };
@@ -109,6 +113,8 @@ const App = () => {
     });
     if (res &&  res.data) {
       setFollowList(res.data)
+    } else {
+      setFollowList([])
     }
   };
 
